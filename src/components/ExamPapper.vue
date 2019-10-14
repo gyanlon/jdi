b6ab7c66ff253a98f96d2cf742c4339ab1483cc8
gyanlon <gyanlon@hotmail.com>
2019-10-14 15:36:11 +0800

    Spported random variables

--- src/components/ExamPapper.vue
+++ src/components/ExamPapper.vue
@@ -1,72 +1,79 @@
 
 <template>
   <div class="questionBody">
-	  <div  v-for="q in questions">
-		  <InputQ :id="q.id" :score="q.score" :content="q.pattern" :variables="q.variables" :answers="q.answers" @change="change" :submit='submit'/>
- 	  </div>
+    <div v-for="q in questions" :key="q">
+      <InputQ
+        :id="q.id"
+        :score="q.score"
+        :content="q.pattern"
+        :variables="q.variables"
+        :answers="q.answers"
+        @change="change"
+        :submit="submit"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-	import InputQ from "./InputQ.vue";
-	import Store from "./store.js"
+import InputQ from "./InputQ.vue";
+import Store from "./store.js";
 
-	export default {
-		props: {
-			submit: false
-		},
+export default {
+  props: {
+    submit: false
+  },
 
-		components: {
-			InputQ
-		},
-		
-		data: function() {
-			return {
-				total: 0,
-				result: {},
-				questions: Store.fetch()
-			};
-		},
+  components: {
+    InputQ
+  },
 
-		methods: {
-			calculateTotal() {
-				var qs = document.getElementsByClassName("question");
-				var i;
-				this.total = 0;
-				for (i = 0; i < qs.length; i++) {
-						var score = qs[i].getAttribute("score");
-						if( score ) {
-							this.total += parseInt(score);
-						}
-				}
+  data: function() {
+    return {
+      total: 0,
+      result: {},
+      questions: Store.fetch()
+    };
+  },
 
-				// console.log(this.total);
-				return this.total;
-			},
-			 
-			caculateScore(result) {
-				var score = 0;
-				for (const [key, value] of Object.entries(result)) {
-					score += parseInt(value);
-				}
+  methods: {
+    calculateTotal() {
+      var qs = document.getElementsByClassName("question");
+      var i;
+      this.total = 0;
+      for (i = 0; i < qs.length; i++) {
+        var score = qs[i].getAttribute("score");
+        if (score) {
+          this.total += parseInt(score);
+        }
+      }
 
-				return score;
-			},
+      // console.log(this.total);
+      return this.total;
+    },
 
-			change(res) {
-				console.log("GET event:", JSON.stringify(res));
-				this.result[res.qid] = res.score;
+    caculateScore(result) {
+      var score = 0;
+      for (const [key, value] of Object.entries(result)) {
+        score += parseInt(value);
+      }
 
-				var response = {
-					total: this.calculateTotal(),
-					score: this.caculateScore(this.result)
-				}
-				this.$emit('change', response);
-			}
-		}
-	};
+      return score;
+    },
+
+    change(res) {
+      console.log("GET event:", JSON.stringify(res));
+      this.result[res.qid] = res.score;
+
+      var response = {
+        total: this.calculateTotal(),
+        score: this.caculateScore(this.result)
+      };
+      this.$emit("change", response);
+    }
+  }
+};
 </script>
 
 <style>
-
 </style>
--- src/components/store.js
+++ src/components/store.js
@@ -1,4 +1,4 @@
-const STORAGE_KEY='questions'
+const STORAGE_KEY='questions';
 
 {/* <InputQ id="q1" score=5 content="{} X {} = ()" variables="3,5" answers="15" @change="change" :submit='submit'/>
 <InputQ id="q2" score=20 content="{} X {} 表示 () 的 () 倍， 又表示()个() 。" variables="8,9" answers="8,9,9,8" @change="change" :submit='submit'/> */}
@@ -13,15 +13,89 @@ const init_questions=[{
     score: 5,
     pattern: "{} X {} = ()",
     variables: "3,5",
-    answers: "15"
+    answers: "15",
+    variable_extent: ["1-9", "1-9"],
+    answer_alg: [ "$[1]*$[2]" ]
 },{ 
     id: "q2",
     score: 20,
     pattern: "{} X {} 表示 () 的 () 倍， 又表示()个() 。",
     variables: "8,9",
-    answers: "8,9,9,8"
+    answers: "8,9,9,8",
+    variable_extent: ["1-9", "1-9"],
+    answer_alg: [ "$[1]", "$[2]", "$[2]", "$[1]" ]
 }];
 
+function init() {
+    console.log("init");
+    for(var i=0; i<init_questions.length; i++) {
+        var q = init_questions[i];
+        // populate variables according to var constrains
+        var v = "";
+        console.log(q.variable_extent);
+        for ( var n=0; n<q.variable_extent.length; n++) {
+            v += getRandomValueInConstrain( q.variable_extent[n] ) + ",";
+        }
+        q.variables = v.substr(0, v.length -1);
+        console.log(q.variables);
+
+        // populate answers accoring to alg
+        q.answers = getAnswerByAlg(q.variables, q.answer_alg);       
+    }
+}
+
+function getAnswerByAlg(vars, algs) {
+    var result = "";
+
+    var answersArr = getArgs(vars, algs);
+    for(var i=0; i<answersArr.length; i++) {
+        result += answersArr[i] + ","
+    }
+    result = result.substr(0, result.length -1);
+
+    console.log("answers:", result);
+    return result;
+}
+
+function getArgs(vars, algs) {
+    var result = [];
+
+    for (var i=0; i<algs.length; i++) {
+
+        // 这是一段神秘的代码， 调整数据的下标, 因为用户习惯时下标从1开始
+        var adjustArr = [];
+        var varArr = vars.split(",");
+        for(var n=0; n<varArr.length; n++) {
+            adjustArr[n+1] = varArr[n];
+        }
+
+        // 这是一段神秘的代码， eval 传参
+        var exp = algs[i].replace(/\$/g, "adjustArr");
+        console.log("exp=", exp, "adjustArr", adjustArr);
+        result[i] = window.eval.call(window,'(function (adjustArr) { return '+ exp +'; })')(adjustArr);
+    }
+
+    console.log("args:", result);
+    return result;
+}
+
+function getRandomValueInConstrain( ex ) {
+    console.log("ex=", ex);
+    var random = ex;
+    
+    if(ex.indexOf("-") > 0) {
+        random = randomFrom(parseInt(ex.split("-")[0]), parseInt(ex.split("-")[1]));
+    }
+
+    console.log("random:", random);
+    return random;
+}
+
+function randomFrom(lowerValue,upperValue) {
+    return Math.floor(Math.random() * (upperValue - lowerValue + 1) + lowerValue);
+}
+
+init();
 window.localStorage.setItem(STORAGE_KEY,JSON.stringify(init_questions));
 
 export default{
