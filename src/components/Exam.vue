701355340ab62005fa1e259797f0d5d2fb8e26a4
gyanlon <gyanlon@hotmail.com>
2019-10-14 20:07:36 +0800

    Added a cow

--- src/components/Exam.vue
+++ src/components/Exam.vue
@@ -13,7 +13,7 @@
 
     <div class="container result" v-if="submit">
       <p>考试得分：{{resultMessage}}<p>
-      <h3 v-if="score===100">不错哦</h3>
+      <h3 v-if="score===100">不错哦！ 🐂</h3>
       <h3 v-if="score>=80&&score<100">加油，再细心一点，你可以得到100分的！</h3>
       <h3 v-if="score>60&&score<80">有点难度吧，不服气就<router-link to="/menu"> 点我 </router-link>再来一遍。</h3>
       <h3 v-if="score<60">失败是成功之母，错题是最好的老师，来看看我们错在哪儿了吧。<p/>不服气就<router-link to="/menu"> 点我 </router-link>再来一遍!</h3>  
