c1d3cad164d49347b905e824071afaaa5b9102ff
gyanlon <gyanlon@hotmail.com>
2019-10-17 19:26:42 +0800

    Added adm

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
       <router-link to="/exam"><h2 class="ab_title" style=" color: blue; cursor: wait; ">开始吧</h2></router-link>
+      <div style="margin-top:1000px">
+      <router-link to="/adm" ><h2 class="ab_title" style=" color: blue; cursor: wait; ">&nbsp;</h2></router-link>
+      </div>
     </div>
  </template>
 <script>
